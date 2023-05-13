a83af27103184bb888688e4ef37d49a115055b9a
Maxim Urschumzew <u.maxim@live.de>
2023-05-13 11:39:06 +0200

    Change serialization framework for ReshapingConfigs.

--- dpsa4flower/reshaping_config.py
+++ dpsa4flower/reshaping_config.py
@@ -1,25 +1,35 @@
 
-import ast
+from dataclasses import dataclass
 from typing import Dict, Optional, Tuple, List
 from flwr.common.typing import Scalar
+from dataclass_wizard import JSONWizard
 
 Shapes = List[Tuple[int, ...]]
 SplitIndices = List[int]
 
-class ReshapingConfig:
-    def __init__(self, shapes: Shapes, split_indices: SplitIndices) -> None:
-        self.shapes = shapes
-        self.split_indices = split_indices
+@dataclass
+class ReshapingConfig(JSONWizard):
+    shapes: Shapes
+    split_indices: SplitIndices
+
+    # def __init__(self, shapes: Shapes, split_indices: SplitIndices) -> None:
+    #     self.shapes = shapes
+    #     self.split_indices = split_indices
 
 def readReshapingConfig(d: Dict[str, Scalar]) -> Optional[ReshapingConfig]:
     reshaping_config_encoded = d.get('reshaping_config')
 
     if isinstance(reshaping_config_encoded, str):
-        return ast.literal_eval(reshaping_config_encoded)
+        c = ReshapingConfig.from_json(reshaping_config_encoded)
+        if isinstance(c, ReshapingConfig):
+            return c
+        else:
+            print("WARNING: Got a list instead of a single object! (when deserializing a ReshapingConfig)")
+            return None
     else:
         return None
 
 def writeReshapingConfig(d: Dict[str, Scalar], c: ReshapingConfig) -> Dict[str, Scalar]:
-    d['reshaping_config'] = repr(c)
+    d['reshaping_config'] = c.to_json()
     return d
 
